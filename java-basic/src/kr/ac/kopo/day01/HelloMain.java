f37cde29213ed69e92a03f5761a642c0c9af9fbf
DESKTOP-O3LMC10\User <jim0120@naver.com>
2024-03-05 11:54:49 +0900

    20240305 오전수업

---
+++ java-basic/src/kr/ac/kopo/day01/HelloMain.java
@@ -0,0 +1,17 @@
+package kr.ac.kopo.day01;
+
+public class HelloMain {
+	public static void main(String[] args) {
+	
+		System.out.print(3);
+		System.out.print(' ');
+		System.out.print('+');
+		System.out.print(' ');
+		System.out.print(4);
+		System.out.println();
+		System.out.println(3 + " + " + 4);
+		System.out.printf("[%4d] %c %d\n", 3, '+' ,4);
+		System.out.printf("[%-4d] %c %d\n", 3, '+' ,4);
+		System.out.printf("[%04d] %c %d\n", 3, '+' ,4);	
+	}
+}
