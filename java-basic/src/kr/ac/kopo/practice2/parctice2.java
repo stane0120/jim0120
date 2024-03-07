036c32d3b5dab2d4ed4d493c1c9df452e3015c15
DESKTOP-RG9NROQ\JINHYUN <jim0120@naver.com>
2024-03-07 22:05:26 +0900

    과제

---
+++ java-basic/src/kr/ac/kopo/practice2/parctice2.java
@@ -0,0 +1,11 @@
+package kr.ac.kopo.practice2;
+
+public class parctice2 {
+	public static void main(String[] args) {
+		for(int i = 100; i >= 1 ; i-=1) {
+			
+			System.out.print(i + " ");
+		}
+	}
+
+}
---
+++ java-basic/src/kr/ac/kopo/practice2/parctice5.java
@@ -0,0 +1,26 @@
+package kr.ac.kopo.practice2;
+
+import java.util.Scanner;
+
+public class parctice5 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("2 ~ 10 사이의 정수 입력 : ");
+		int i = sc.nextInt();
+		
+		    if(i < 2 || i > 10) {
+			System.out.print("2 ~ 10 사이의 정수를 입력해주세요");
+		    } else {
+		
+		    for(int a = 1 ; a <= 100 ; a++) {		
+              if( a % i != 0) {
+				System.out.print("");
+			} else {
+				System.out.print(a + " ");
+			}
+		}
+	}
+}
+}
+
