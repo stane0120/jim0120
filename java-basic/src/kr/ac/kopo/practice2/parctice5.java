c3001436c2209dc82b783a031dff7a3bdd6c6d09
DESKTOP-RG9NROQ\JINHYUN <jim0120@naver.com>
2024-03-09 13:54:03 +0900

    11

--- java-basic/src/kr/ac/kopo/practice2/parctice5.java
+++ java-basic/src/kr/ac/kopo/practice2/parctice5.java
@@ -4,29 +4,26 @@ import java.util.Scanner;
 
 public class parctice5 {
 	public static void main(String[] args) {
-		
+
 		try (Scanner sc = new Scanner(System.in)) {
 			System.out.print("2 ~ 10 사이의 정수 입력 : ");
 			int i = Integer.parseInt(sc.nextLine());
-			
-			    if(i < 2 || i > 10) {
+
+			if (i < 2 || i > 10) {
 				System.out.print("2 ~ 10 사이의 정수를 입력해주세요");
-				
-			    } else {
-			
-			    for(int a = 1 ; a <= 100 ; a++) {		
-			      if( a % i != 0) {
-					System.out.print("");
-				} else {
-					System.out.print(a + " ");
+
+			} else {
+
+				for (int a = 1; a <= 100; a++) {
+					if (a % i != 0) {
+						System.out.print("");
+					} else {
+						System.out.print(a + " ");
+					}
 				}
 			}
-}
-		} catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
+	}
 }
-	} 
 
 
--- java-basic/src/kr/ac/kopo/practice2/parctice6.java
+++ java-basic/src/kr/ac/kopo/practice2/parctice6.java
@@ -7,26 +7,36 @@ public class parctice6 {
 		try (Scanner sc = new Scanner(System.in)) {
 			int[] a = new int[5];
 
-			    for (int i = 0; i < 5; i++) {
-			        System.out.print((i + 1) + "'s 성적: ");
-			        a[i] = sc.nextInt();
+			for (int i = 0; i < 5; i++) {
+				System.out.print((i + 1) + "'s 성적: ");
+				a[i] = sc.nextInt();
 
-			        char b;
-			        switch (a[i] / 10) {
-			            case 10:
-			            case 9: b = 'A'; break;
-			            case 8: b = 'B'; break;
-			            case 7: b = 'C'; break;
-			            case 6: b = 'D'; break;
-			            default: b = 'F'; break;
-			        }
-			        if (a[i] < 0 || a[i] > 100) {
-			            System.out.println("잘못 입력하셨습니다.");
-			        } else {
-			            System.out.println(a[i] + "점의 학점은 " + b + "입니다.");
-			        }
-			    }
+				char b;
+				switch (a[i] / 10) {
+				case 10:
+				case 9:
+					b = 'A';
+					break;
+				case 8:
+					b = 'B';
+					break;
+				case 7:
+					b = 'C';
+					break;
+				case 6:
+					b = 'D';
+					break;
+				default:
+					b = 'F';
+					break;
+				}
+				if (a[i] < 0 || a[i] > 100) {
+					System.out.println("잘못 입력하셨습니다.");
+				} else {
+					System.out.println(a[i] + "점의 학점은 " + b + "입니다.");
+				}
+			}
 		}
-		}
-	    }
+	}
+}
 	
