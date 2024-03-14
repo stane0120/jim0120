1dac7b9c8d5a7e091daae0ad972a66ec06b8b967
DESKTOP-O3LMC10\User <jim0120@naver.com>
2024-03-14 11:48:28 +0900

    20240314 오전

--- java-basic/src/kr/ac/kopo/day08/CarMain.java
+++ java-basic/src/kr/ac/kopo/day08/CarMain.java
@@ -7,6 +7,14 @@ public class CarMain {
 		
 		Car c = new Car();
 		Car c2 = new Car();
-	}
 
+		c.name = "소나타";
+		c.price = 3600;
+		
+		c2.name = "그랜저";
+		c2.price = 4200;
+		
+		System.out.println("자동차명 : " + c.name + ", 가격 : " + c.price + "만원");
+		System.out.println("자동차명 : " + c2.name + ", 가격 : " + c2.price + "만원");
+	}
 }
