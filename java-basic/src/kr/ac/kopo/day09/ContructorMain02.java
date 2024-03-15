7f86188fcfa7a7920b327a4ff618c3f1d387fbaf
DESKTOP-O3LMC10\User <jim0120@naver.com>
2024-03-15 15:52:53 +0900

    20240315 오후

---
+++ java-basic/src/kr/ac/kopo/day09/ContructorMain02.java
@@ -0,0 +1,17 @@
+package kr.ac.kopo.day09;
+
+public class ContructorMain02 {
+	
+	public static void main(String[] args) {
+		
+		Member m = new Member();
+		Member m2 = new Member("홍길동");
+		Member m3 = new Member("고길동", 30);
+		Member m4 = new Member("강길동", 24, "AB");
+		
+		m.info();
+		m2.info();
+		m3.info();
+	    m4.info();
+	}
+}
