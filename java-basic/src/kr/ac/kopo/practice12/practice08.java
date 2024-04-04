01b2d95fffbe65393603aab2f10db3213cd0d647
DESKTOP-RG9NROQ\JINHYUN <jim0120@naver.com>
2024-04-04 23:17:33 +0900

    1

--- java-basic/src/kr/ac/kopo/practice12/practice08.java
+++ java-basic/src/kr/ac/kopo/practice12/practice08.java
@@ -4,19 +4,34 @@ import java.util.Scanner;
 
 public class practice08 {
     public static void main(String[] args) {
-    	Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        
-        // 초기 상태의 점의 수는 4개입니다.
-        int points = 4;
-        
-        // N번의 과정을 거치면서 정사각형과 점의 수를 업데이트합니다.
-        for (int i = -2; i <= N; i++) {
-            // 현재 점의 개수에서 새로운 정사각형이 추가될 때마다, 각 정사각형의 변의 중점과 정사각형의 중심에 대한 점을 추가합니다.
-        	points += (i * i) + ((i) * (i));
-        }
-        
-        System.out.println(points);
-        scanner.close();
-    }
-}
+    	 Scanner sc = new Scanner(System.in);
+
+         int n = sc.nextInt();
+         int answer = 4;
+
+         // 메모이제이션 테이블 생성
+         int[] memo = new int[n + 1];
+         memo[1] = 4;
+
+         // 1부터 n까지 반복
+         for (int i = 2; i <= n; i++) {
+             // 각 변의 중앙에 점 추가
+             memo[i] *= 2;
+
+             // 홀수 번 과정에서 정사각형 중심에 점 추가
+             if (i % 2 == 1) {
+                 memo[i] += 1;
+             }
+
+             // 중복되는 점 제거
+             for (int j = 1; j < i; j++) {
+                 if (i % j == 0) {
+                     memo[i] -= memo[j];
+                 }
+             }
+         }
+
+         // 결과 출력
+         System.out.println(memo[n]);
+     }
+ }
