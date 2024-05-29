1c98a82a1b0810423d98a1584764a1515478a36d
DESKTOP-O3LMC10\User <hsim0203@naver.com>
2024-05-29 17:37:20 +0900

    2

--- OracleDB 연습/테이블.sql
+++ OracleDB 연습/테이블.sql
@@ -20,3 +20,23 @@ VALUES(seq_t_board_no.nextval, '죽을만큼아파서', '고길동', '지옥같�
 COMMIT;
 
 SELECT * FROM t_board;
+
+CREATE TABLE T_MEMBER(
+    ID 			VARCHAR2(20) PRIMARY KEY,
+    NAME 		VARCHAR2(20) NOT NULL,
+    PASSWORD 		VARCHAR2(20) NOT NULL,
+    EMAIL_ID 		VARCHAR2(30),
+    EMAIL_DOMAIN 	VARCHAR2(20),
+    TEL1 			CHAR(3),
+    TEL2 			CHAR(4),  
+    TEL3 			CHAR(4),
+    POST 			CHAR(6),
+    BASIC_ADDR 		VARCHAR2(200),
+    DETAIL_ADDR 		VARCHAR2(200),
+    TYPE            		CHAR(1) default 'U',
+    REG_DATE 		DATE default sysdate
+);
+
+COMMIT;
+
+SELECT * FROM T_MEMBER;
